bf9e5f3c25abaaff3005067f24e9ec63e2b369bf
Terminus <i.can.speak.c.and.basic@gmail.com>
2013-04-19 16:55:10 +0900

    Fixed some bugs.
    Now it runs properly.

--- MkLine.py
+++ MkLine.py
@@ -7,37 +7,35 @@ OneLine=X*(Ceilings+1)/2
 
 ar=[-1]*(X-2)
 #res=[[]]*((OneLine-1-2)-(OneLine-Ceilings-Ceilings-1))
-res=[]
+res=[[] for i in range((OneLine-1-2)-(OneLine-Ceilings-(Ceilings-1)) +1 )]
 dned=[False]*Ceilings
 
 def MkLine(level):
 	if level==X-2:
 		chk_and_add()
 	else:
-		for i in range(0, Ceilings):
-			if dned[i]:
+		for i in range(1, Ceilings+1):
+			if dned[i-1]:
 				continue
 			ar[level]=i
-			dned[i]=True
+			dned[i-1]=True
 			MkLine(level+1)
-			dned[i]=False
+			dned[i-1]=False
 
 def chk_and_add():
 	sum=0
 	for i in ar:
 		sum+=i
-	if (OneLine-Ceilings-Ceilings-1)<=sum and sum<=(OneLine-1-2):
-		res[sum-1-(OneLine-Ceilings-Ceilings-1)].append([-1]*(X-2))
-		print res
+	if (OneLine-Ceilings-(Ceilings-1))<=sum and sum<=(OneLine-1-2):
+		res[sum-(OneLine-Ceilings-(Ceilings-1))].append([-1]*(X-2))
+		l=len(res[sum-(OneLine-Ceilings-(Ceilings-1))])
 		for i in range(0, X-2):
-			res[sum-1-(OneLine-Ceilings-Ceilings-1)][i]=ar[i]
-		#res[sum-1-(OneLine-Ceilings-Ceilings-1)].append(ar)
-		#print ar
+			res[sum-(OneLine-Ceilings-(Ceilings-1))][l-1][i] = ar[i]
 
 def outar():
 	os.chdir("out")
-	for i in range(OneLine-Ceilings-Ceilings-1, OneLine-1-2+1):
-		f=open(str(i), "w")
+	for i in range(0, (OneLine-1-2)-(OneLine-Ceilings-(Ceilings-1))+1):
+		f=open(str(i+(OneLine-Ceilings-(Ceilings-1))), "w")
 		for j in res[i]:
 			for k in j:
 				f.write(str(k)+"\t")
@@ -45,9 +43,6 @@ def outar():
 		f.close()
 
 def mine():
-	for i in range(0, (OneLine-1-2)-(OneLine-Ceilings-Ceilings-1)):
-		res.append([])
-	print res
 	MkLine(0)
 	outar()
 
